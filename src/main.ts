38d0427128416205146704f383e5a656bc3f1179
Bakytbek314 <spidybats72@gmail.com>
2025-04-27 21:47:12 +0600

    fix_static

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import *as bodyParser from 'body-parser';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  app.useStaticAssets(join(__dirname, '..', 'static'), {
+    prefix: '/',
+  });
   await app.listen(5000);
 }
 bootstrap();
