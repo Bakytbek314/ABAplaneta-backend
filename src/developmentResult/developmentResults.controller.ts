e570217b4c5a67f3bbfd46eba7de575f4ab6fb53
Bakytbek314 <spidybats72@gmail.com>
2026-03-10 14:07:49 +0600

    vercel2

--- src/developmentResult/developmentResults.controller.ts
+++ src/developmentResult/developmentResults.controller.ts
@@ -1,10 +1,6 @@
 import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { DevelopmentResultsService } from "./developmentResults.service";
 import { DevelopmentResultDto } from "./dto/developmentResult.dto";
-import { Role } from "src/auth/role/role.enum";
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { Roles } from "src/auth/role/role.decorator";
-import { RolesGuard } from "src/auth/role/roles.guard";
 
 @Controller("development")
 export class DevelopmentResultsController {
